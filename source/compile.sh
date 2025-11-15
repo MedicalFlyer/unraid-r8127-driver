b210b214abe1a82829a8f50e93523ef7d9ba5a61
MedicalFlyer <104535443+MedicalFlyer@users.noreply.github.com>
2025-11-15 17:00:44 +0000

    8127 support test

--- source/compile.sh
+++ source/compile.sh
@@ -1,7 +1,6 @@
 # Clone repository and get latest commit
 cd ${DATA_DIR}
-git clone https://github.com/jinlife/unraid-r8125-r8152-driver
-cd ${DATA_DIR}/unraid-r8125-r8152-driver
+git clone https://github.com/MedicalFlyer/unraid-r8127-drivercd ${DATA_DIR}/unraid-r8125-r8152-driver
 PLUGIN_VERSION="$(git log -1 --format="%cs" | sed 's/-//g')"
 git checkout main
 
@@ -29,8 +28,7 @@ tee $TMP_DIR/$VERSION/install/slack-desc <<EOF
        |-----handy-ruler------------------------------------------------------|
 $PLUGIN_NAME: $PLUGIN_NAME OOT driver by jinlife
 $PLUGIN_NAME:
-$PLUGIN_NAME: Source: https://github.com/jinlife/unraid-r8125-r8152-driver
-$PLUGIN_NAME:
+$PLUGIN_NAME: Source: https://github.com/MedicalFlyer/unraid-r8127-driver$PLUGIN_NAME:
 $PLUGIN_NAME: Custom $PLUGIN_NAME driver package for Unraid Kernel v${UNAME%%-*} by ich777
 $PLUGIN_NAME:
 EOF
@@ -62,8 +60,7 @@ tee $TMP_DIR/$VERSION/install/slack-desc <<EOF
        |-----handy-ruler------------------------------------------------------|
 $PLUGIN_NAME: $PLUGIN_NAME OOT driver by jinlife
 $PLUGIN_NAME:
-$PLUGIN_NAME: Source: https://github.com/jinlife/unraid-r8125-r8152-driver
-$PLUGIN_NAME:
+$PLUGIN_NAME: Source: https://github.com/MedicalFlyer/unraid-r8127-driver$PLUGIN_NAME:
 $PLUGIN_NAME: Custom $PLUGIN_NAME driver package for Unraid Kernel v${UNAME%%-*} by ich777
 $PLUGIN_NAME:
 $PLUGIN_NAME:
@@ -99,8 +96,7 @@ tee $TMP_DIR/$VERSION/install/slack-desc <<EOF
        |-----handy-ruler------------------------------------------------------|
 $PLUGIN_NAME: $PLUGIN_NAME OOT driver by jinlife
 $PLUGIN_NAME:
-$PLUGIN_NAME: Source: https://github.com/jinlife/unraid-r8125-r8152-driver
-$PLUGIN_NAME:
+$PLUGIN_NAME: Source: https://github.com/MedicalFlyer/unraid-r8127-driver$PLUGIN_NAME:
 $PLUGIN_NAME: Custom $PLUGIN_NAME driver package for Unraid Kernel v${UNAME%%-*} by ich777
 $PLUGIN_NAME:
 $PLUGIN_NAME:
@@ -136,8 +132,7 @@ tee $TMP_DIR/$VERSION/install/slack-desc <<EOF
        |-----handy-ruler------------------------------------------------------|
 $PLUGIN_NAME: $PLUGIN_NAME OOT driver by jinlife
 $PLUGIN_NAME:
-$PLUGIN_NAME: Source: https://github.com/jinlife/unraid-r8125-r8152-driver
-$PLUGIN_NAME:
+$PLUGIN_NAME: Source: https://github.com/MedicalFlyer/unraid-r8127-driver$PLUGIN_NAME:
 $PLUGIN_NAME: Custom $PLUGIN_NAME driver package for Unraid Kernel v${UNAME%%-*} by ich777
 $PLUGIN_NAME:
 $PLUGIN_NAME:
@@ -149,3 +144,38 @@ EOF
 ${DATA_DIR}/bzroot-extracted-$UNAME/sbin/makepkg -l n -c n $TMP_DIR/$PLUGIN_NAME-$PLUGIN_VERSION-$UNAME-1.txz
 md5sum $TMP_DIR/$PLUGIN_NAME-$PLUGIN_VERSION-$UNAME-1.txz | awk '{print $1}' > $TMP_DIR/$PLUGIN_NAME-$PLUGIN_VERSION-$UNAME-1.txz.md5
 
+
+# Compile r8127 Kernel Module and install it to the temporary directory "/RTL8127"
+cd ${DATA_DIR}/unraid-r8125-r8152-driver/r8127/src
+
+make -j${CPU_COUNT}
+make INSTALL_MOD_PATH=/RTL8127 install -j${CPU_COUNT}
+
+# Remove non Kernel modules from temporary directory
+rm /RTL8127/lib/modules/${UNAME}/* 2>/dev/null
+
+# Create Slackware package
+PLUGIN_NAME="r8127"
+BASE_DIR="/RTL8127"
+TMP_DIR="/tmp/${PLUGIN_NAME}_"$(echo $RANDOM)""
+VERSION="$(date +'%Y.%m.%d')"
+
+mkdir -p $TMP_DIR/$VERSION
+cd $TMP_DIR/$VERSION
+cp -R $BASE_DIR/* $TMP_DIR/$VERSION/
+mkdir $TMP_DIR/$VERSION/install
+tee $TMP_DIR/$VERSION/install/slack-desc <<EOF
+       |-----handy-ruler------------------------------------------------------|
+$PLUGIN_NAME: $PLUGIN_NAME OOT driver by jinlife
+$PLUGIN_NAME:
+$PLUGIN_NAME: Source: https://github.com/MedicalFlyer/unraid-r8127-driver$PLUGIN_NAME:
+$PLUGIN_NAME: Custom $PLUGIN_NAME driver package for Unraid Kernel v${UNAME%%-*} by ich777
+$PLUGIN_NAME:
+$PLUGIN_NAME:
+$PLUGIN_NAME:
+$PLUGIN_NAME:
+$PLUGIN_NAME:
+$PLUGIN_NAME:
+EOF
+${DATA_DIR}/bzroot-extracted-$UNAME/sbin/makepkg -l n -c n $TMP_DIR/$PLUGIN_NAME-$PLUGIN_VERSION-$UNAME-1.txz
+md5sum $TMP_DIR/$PLUGIN_NAME-$PLUGIN_VERSION-$UNAME-1.txz | awk '{print $1}' > $TMP_DIR/$PLUGIN_NAME-$PLUGIN_VERSION-$UNAME-1.txz.md5
